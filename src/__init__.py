3a692400aa2ab4e1ec0a39fab6c1eb716bd28067
JoseHP <jose.hndz.prz@gmail.com>
2023-04-19 22:43:08 -0400

    Changes to be committed: modified:   LICENSE deleted:    __pycache__/VIAFreffun.cpython-310.pyc deleted:    __pycache__/__init__.cpython-310.pyc deleted:    __pycache__/functionversions.cpython-310.pyc 	deleted:    __pycache__/tagfunctions.cpython-310.pyc 	new file:   dist/xmlcit-0.0.5-py3-none-any.whl 	new file:   dist/xmlcit-0.0.5.tar.gz 	modified:   pyproject.toml 	deleted:    src/VIAF_functions/__init.py 	new file:   src/VIAF_functions/__init__.py 	deleted:    src/XMLCit/__init__.py 	new file:   src/__init__.py 	renamed:    src/XMLCit/addtagfunc.py -> src/addtagfunc.py 	modified:   src/opcit_functions/__init__.py 	renamed:    src/XMLCit/tagfunctions.py -> src/tagfunctions.py

---
+++ src/__init__.py
@@ -0,0 +1,5 @@
+from .addtagfunc import Addtag
+from .tagfunctions import Insert
+from .opcit_functions import RepeatCitation
+from .VIAF_functions import AddVIAF
+
---
+++ src/opcit_functions/__init__.py
@@ -0,0 +1,1 @@
+from .Ibidfunc import RepeatCitation
